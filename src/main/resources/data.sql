962ad05f6dfa7d36ed9ed79ba2989b75bff3cbef
vitor-apolinario <vitorapoli@gmail.com>
2020-03-02 13:51:53 -0300

    criação do service de autenticação, e do UsuarioRepository, autenticação funcionando

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,4 +1,4 @@
-INSERT INTO USUARIO(nome, email, senha) VALUES('Aluno', 'aluno@email.com', '123456');
+INSERT INTO USUARIO(nome, email, senha) VALUES('Aluno', 'aluno@email.com', '$2a$10$qSIg7BFQipzUMA5485QqNOY.tuZBRflUBoy6WxkynIn.39XaP6DvO');
 
 INSERT INTO CURSO(nome, categoria) VALUES('Spring Boot', 'Programação');
 INSERT INTO CURSO(nome, categoria) VALUES('HTML 5', 'Front-end');
